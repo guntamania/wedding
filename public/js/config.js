5310c512849c473f41cba0e8cc149c03d6ce4be8
hiroki yamada <medaka3-lj@infoseek.jp>
2017-04-24 00:33:28 +0900

    change locations of js files

---
+++ public/js/config.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyDt_58IU-QeE51iOCulHeA17blW9DxVZ78",
+    authDomain: "wedding.firebaseapp.com/",
+    databaseURL: "https://wedding-add3c.firebaseio.com/",
+    storageBucket: "wedding.appspot.com"
+};
+firebase.initializeApp(config);
