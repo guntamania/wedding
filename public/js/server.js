5cd326113c9834b2e5040c2f092aa20e83e32e2f
takashimorigaki <takashi.morigaki@8ce.me>
2017-04-26 17:01:47 +0900

    Clean up js

--- public/js/server.js
+++ public/js/server.js
@@ -6,23 +6,23 @@ var database = firebase.database();
 //
 
 if (database) {
-    database.ref('/hee/counts').once('value').then(function(snapshot) {
-	console.log('snapshot'+snapshot.val());
-	var total = sum(snapshot.val());
-	var sliceTotal = ('000'+total).slice(-3);
-	//$('#counter').html("合計:" + total + "へぇ");
-  $('#counter').html(sliceTotal);
+    database.ref('/hee/counts').once('value').then(function (snapshot) {
+        console.log('snapshot' + snapshot.val());
+        var total = sum(snapshot.val());
+        var sliceTotal = ('000' + total).slice(-3);
+        //$('#counter').html("合計:" + total + "へぇ");
+        $('#counter').html(sliceTotal);
     });
 }
 
 if (database) {
     var globalCount = database.ref('/hee/counts');
-    globalCount.on('value', function(snapshot) {
-	console.log('snapshot'+snapshot.val());
-	var total = sum(snapshot.val());
-  var sliceTotal = ('000'+total).slice(-3);
-	//$('#counter').html("合計:" + total + "へぇ");
-  $('#counter').html(sliceTotal);
+    globalCount.on('value', function (snapshot) {
+        console.log('snapshot' + snapshot.val());
+        var total = sum(snapshot.val());
+        var sliceTotal = ('000' + total).slice(-3);
+        //$('#counter').html("合計:" + total + "へぇ");
+        $('#counter').html(sliceTotal);
     });
 }
 
@@ -33,11 +33,11 @@ if (database) {
 function resetAllHees() {
     // increase remote value
     database.ref('hee/reset').once('value').then(
-	function(snapshot) {
-	    database.ref('/hee').set({
-		reset: (snapshot.val() + 1)
-	    });
-	}
+        function (snapshot) {
+            database.ref('/hee').set({
+                reset: (snapshot.val() + 1)
+            });
+        }
     );
 
     database.ref('/hee/counts').removed();
@@ -48,11 +48,11 @@ function resetAllHees() {
 //
 
 function sum(arr) {
-    if(!arr) return 0;
+    if (!arr) return 0;
     var sum = 0;
     console.log('in log' + JSON.stringify(arr));
-    for(var key in arr) {
-	sum += arr[key]['count_for_user'];
+    for (var key in arr) {
+        sum += arr[key]['count_for_user'];
     }
     return sum;
 }
